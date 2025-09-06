3c207ee1825f2d492106e9cec5f042fe78735773
Naveen-PJ <naveenpj4623@gmail.com>
2025-09-06 12:33:51 +0530

    Fixed migration error

--- backend/ecofinds/settings.py
+++ backend/ecofinds/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users'
+    'users',
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
