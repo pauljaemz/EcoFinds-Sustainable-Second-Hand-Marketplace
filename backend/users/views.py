948d15a6db46ac2b23037dfce8029cb1865b2a10
pauljaemz <pauljames2001@gmail.com>
2025-09-06 10:31:23 +0530

    Backend is cleaned up. User app created

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,21 +1,3 @@
-from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
-from .models import User
-from .serializers import UserSerializer, UserProfileSerializer
+from django.shortcuts import render
 
-class UserRegistrationView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-class UserProfileView(generics.RetrieveUpdateAPIView):
-    permission_classes = [IsAuthenticated]
-    queryset = User.objects.all()
-    serializer_class = UserProfileSerializer
-
-class UserLoginView(generics.GenericAPIView):
-    # Implement login logic here
-    pass
-
-class UserLogoutView(generics.GenericAPIView):
-    # Implement logout logic here
-    pass
+# Create your views here.
